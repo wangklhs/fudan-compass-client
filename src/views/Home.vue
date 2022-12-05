baa63e2360452fc186e25ffcf0a7270b4850c911
Yixin_Wu <yixin200810@126.com>
2022-12-05 17:03:45 +0800

    主页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -645,6 +645,7 @@ export default {
   margin: 0 15px;
   padding: 0 25px;
   border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
 }
 
 .el-main {
@@ -655,6 +656,7 @@ export default {
   margin: 0 15px;
   padding: 0 25px;
   border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
 }
 
 .el-card {
